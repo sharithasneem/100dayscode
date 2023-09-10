d15bf030df155605d5095d8b4b6aa737cac49c09
shariThasneem <sharibathasneem@gmail.com>
2023-09-10 21:15:55 +0530

    finished

--- darkMode10/index.js
+++ darkMode10/index.js
@@ -1,12 +1,16 @@
 const input1 = document.querySelector(".input");
 const MainBody = document.querySelector("body");
 let temp = 0;
-input1.addEventListener("input", () => {
-	if (temp == 0) {
+input1.checked = JSON.parse(localStorage.getItem("mode"));
+updated();
+function updated() {
+	if (input1.checked) {
 		MainBody.style.backgroundColor = "black";
-		temp = 1;
 	} else {
 		MainBody.style.backgroundColor = "white";
-		temp = 0;
 	}
+}
+input1.addEventListener("input", () => {
+	updated();
+	localStorage.setItem("mode", JSON.stringify(input1.checked));
 });
